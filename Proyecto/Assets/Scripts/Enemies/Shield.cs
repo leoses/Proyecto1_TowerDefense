0e2503f56535f0dff2c696436f89d4d73e448acc
pamart24 <48999523+pamart24@users.noreply.github.com>
2019-03-31 16:51:49 +0200

    Añadido el Escudo

---
+++ Proyecto/Assets/Scripts/Enemies/Shield.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        BalaEscopeta esc = collision.gameObject.GetComponent<BalaEscopeta>();
+        Destroy(esc.gameObject);
+    }
+}
