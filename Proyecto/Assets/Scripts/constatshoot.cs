6282640fe8140ec046343b79888ca0ebbb9b0dbc
auroraGJ <48923631+auroraGJ@users.noreply.github.com>
2019-04-15 15:57:22 +0200

    Añadido script para que la pistola pueda disparar constantemente
    dispara constantemente si se mantiene el boton presionado

---
+++ Proyecto/Assets/Scripts/constatshoot.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class constatshoot : MonoBehaviour {
+    public AudioClip sound;
+    AudioSource source;
+
+    //Disparo de la pistola
+    public int damage;
+    float tiempo;
+    public float cadencia =1;
+    public Bullet bala;
+    private GameObject pool;
+    private Animator animator;
+    private float n= 1;
+    // Use this for initialization
+    void Start ()
+    {
+       
+        
+        source = gameObject.GetComponentInParent<AudioSource>();
+        pool = GameObject.FindGameObjectWithTag("bulletpool");
+        tiempo = cadencia;
+        animator = this.transform.parent.GetComponentInChildren<Animator>();
+        //InvokeRepeating("Shooting", 0, n);
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //while(Input.GetMouseButtonDown(0) && )
+        if (Input.GetMouseButtonDown(0))
+        {
+            n = cadencia;
+            InvokeRepeating("Shooting", 0, n);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            CancelInvoke();
+        }
+	}
+
+    void Shooting()
+    {
+        animator.SetTrigger("Shoot");
+        Bullet balaNueva = Instantiate<Bullet>(bala, transform.position, transform.rotation, pool.transform);
+        balaNueva.newValues(-transform.up, damage);
+        source.clip = sound;
+        source.Play();
+    }
+}
