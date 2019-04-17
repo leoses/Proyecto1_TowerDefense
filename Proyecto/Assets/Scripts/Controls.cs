5a94ed1adb6f0e1a9dca2c6b16595d1f9ea0ea84
auroraGJ <48923631+auroraGJ@users.noreply.github.com>
2019-04-17 18:38:10 +0200

    Menu de los controles casi terminado
    Faltan algunos detalles, pero el menu funciona

---
+++ Proyecto/Assets/Scripts/Controls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Controls : MonoBehaviour {
+
+    public GameObject Movement, MeleeAtack, Turret, Barricade, Shoot, WeaponChange;
+	// Use this for initialization
+	void Start ()
+    {
+        Movement.SetActive(false);
+        Turret.SetActive(false);
+        MeleeAtack.SetActive(false);
+        Barricade.SetActive(false);
+        Shoot.SetActive(false);
+        WeaponChange.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Activa(GameObject objeto)
+    {
+        objeto.SetActive(true);
+    }
+    public void Desactiva(GameObject objeto)
+    {
+        objeto.SetActive(false);
+    }
+
+    public void UpdateScene(string scene)
+    {
+        GameManager.instance.ChangeScene(scene);
+    }
+
+}
