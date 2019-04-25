f0b4488886fff5efaec28a9ddc1adf99aebc7e8d
georgimednikov <gmedniko@ucm.es>
2019-04-25 19:30:46 +0200

    Update Direccion.cs

--- Proyecto/Assets/Scripts/Proyectiles/PlayerShooting/Direccion.cs
+++ Proyecto/Assets/Scripts/Proyectiles/PlayerShooting/Direccion.cs
@@ -13,27 +13,39 @@ public class Direccion : MonoBehaviour {
         cam = Camera.main;
     }
     // Update is called once per frame
-    void OnGUI()
-    {
-        //Para calcular la posición del ratón en el mundo (según la cámara)
-        Event currentEvent = Event.current;
-        mousePos = new Vector2();
-        mousePos.x = currentEvent.mousePosition.x;
-        mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
-        point = new Vector3();
-        //point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
-        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.farClipPlane));
-        mouse = new Vector3(0, 0, 1);
 
-        //Debug.Log(point + " y " + mousePos.y);
+    /*
+void OnGUI()
+{
+    //Para calcular la posición del ratón en el mundo (según la cámara)
+    Event currentEvent = Event.current;
+    mousePos = new Vector2();
+    mousePos.x = currentEvent.mousePosition.x;
+    mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+    point = new Vector3();
+    //point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+    point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.farClipPlane));
+    mouse = new Vector3(0, 0, 1);
 
-    }
+    //Debug.Log(point + " y " + mousePos.y);
+
+}
 
 
-    private void Update()
-    {
-        if (Time.timeScale > 0)
-            transform.LookAt(point, mouse);
-    }
+private void Update()
+{
+    if (Time.timeScale > 0)
+        transform.LookAt(point, mouse);
 
+
+}
+    /*/
+        void Update()
+        {
+            point = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.farClipPlane));
+            Vector2 ang = new Vector2(point.x - transform.position.x, point.y - transform.position.y);
+            float newang = Mathf.Rad2Deg * Mathf.Atan2(ang.y, ang.x);
+            transform.rotation = Quaternion.Euler(0, 0, newang + 90);
+        }
+        //*/
 }
