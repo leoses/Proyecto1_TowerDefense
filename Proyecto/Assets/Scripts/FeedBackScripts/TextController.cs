767eaf051f82c856ee2306b310e3e3baa55cc554
leoses <leoses@ucm.es>
2019-05-01 11:38:32 +0200

    Añadido feedback al colocar torretas y barricadas

---
+++ Proyecto/Assets/Scripts/FeedBackScripts/TextController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+    public float seconds;
+    private Text text;
+    public float speed;
+    private Vector3 direction = new Vector3(0, 1, 0);
+
+	void Start () {
+        text = gameObject.GetComponent<Text>();
+        Destroy(this.gameObject, seconds);
+	}
+
+    private void Update()
+    {
+        transform.Translate(speed * direction * Time.deltaTime);
+    }
+
+
+    public void ModifyText(string newText)
+    {
+        if (text == null) text = this.gameObject.GetComponent<Text>();
+        text.text = newText;
+    }
+}
